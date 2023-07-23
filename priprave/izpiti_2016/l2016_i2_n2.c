2c2d6e6a082774af6e4ab5652a604695b68d1d0e
Jan Jure Stojanovič <janjures@gmail.com>
2023-07-23 11:53:18 +0200

    Naredil nalogo, looks good

--- priprave/izpiti_2016/l2016_i2_n2.c
+++ priprave/izpiti_2016/l2016_i2_n2.c
@@ -3,14 +3,18 @@
 #include <stdlib.h>
 
 int main() {
+    
+    // Dimenzije tabele in dolzina
     int p, q, d;
     scanf("%d %d %d", &p, &q, &d);
     
+    // Ustvarimo tabelo
     int** t = malloc(p*sizeof(int*));
     for (int i = 0; i < p; i++) {
         t[i] = malloc(q*sizeof(int));
     }
     
+    // Shranimo stevilke v tabelo
     for (int i = 0; i < p; i++) {
         for (int j = 0; j < q; j++) {
             scanf("%d", &t[i][j]);
@@ -26,31 +30,52 @@ int main() {
     }
     */
     
-    int trenutna = 0;
-    int naslednja = 1;
-    int razdalja;
+    int x1;
+    int y1;
     
-    while (d > 0 || trenutna != p*q - 1) {
-        for (int i = 0; i < p; i++) {
-            for (int j = 0; j < q; j++) {
-                // Najdemo a
-                if (t[i][j] == trenutna) {
-                    for (int k = 0; k < p; k++) {
-                        for (int l = 0; l < q; l++) {
-                            // Najdemo b
-                            if (t[k][l] == naslednja) {
-                                // Izracunamo razdaljo med a in b
-                                razdalja = abs(i - k) + abs(j - l);
-                            }
-                        }
-                    }
-                }
+    int x2;
+    int y2;
+    
+    for (int i = 0; i < p; i++) {
+        for (int j = 0; j < q; j++) {
+            if (t[i][j] == 0) {
+                x1 = i;
+                y1 = j;
             }
         }
-        d = d - razdalja;
+    }    
+    
+    int raz;
+    
+    int kjerSmo = 0;
+    int naslednja = 1;
+    
+    while (d >= 0 && naslednja <= (p*q - 1)) {
+        for (int i = 0; i < p; i++) {
+            for (int j = 0; j < q; j++) {
+                if (t[i][j] == naslednja) {
+                    x2 = i;
+                    y2 = j;
+                }
+            }
+        } 
+        
+        // Razdalja med tockama
+        raz = abs(x2 - x1) + abs(y2 - y1);
+        // Odstejemo razdaljo od goriva
+        d -= raz;
+        
+        if (d >= 0) {
+            kjerSmo += 1;
+        }
+        
+        // Naslednja tocka kamor gremo
+        naslednja += 1;
+        
+        x1 = x2;
+        y1 = y2;
     }
     
-    printf("%d\n", trenutna);
+    printf("%d\n", kjerSmo);
 }
 
-// Test
