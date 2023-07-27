ddcccbc92baafb4b9209fe4e2d675afc81b3d103
Jan Jure Stojanovič <janjures@gmail.com>
2023-07-27 10:03:05 +0200

    Skoraj narejena naloga 2 iz izpita 3 leto 2016, ne dela na velikih primerih

---
+++ priprave/izpiti_2016/l2016_i3_n2.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+int main() {
+        
+    // Allociramo dovolj prostora za stevili
+    int* a = calloc(10000, sizeof(int));
+    int* b = calloc(10000, sizeof(int));
+        
+    // Shranimo stevilo a
+    int i = 0;
+    char x = getchar();
+    while(x != '\n') {
+        a[i] = x - '0';
+        i++;
+        x = getchar();
+    }
+    
+    // Shranimo stevilo b
+    int j = 0;
+    char y = getchar();
+    while(y != '\n') {
+        b[j] = y - '0';
+        j++;
+        y = getchar();
+    }
+    
+    // Prostor kjer bo shranjena vsota
+    int* c = calloc(10000, sizeof(int));
+    
+    // Allociramo dovolj prostora za stevili
+    int* aa = calloc(10000, sizeof(int));
+    int* bb = calloc(10000, sizeof(int));
+    
+    for (int k = 9999; k >= 0; k--) {
+        aa[k] = a[i];
+        i--;
+        if (i < 0) {
+            break;
+        }
+    }
+    
+    for (int k = 9999; k >= 0; k--) {
+        bb[k] = b[j];
+        j--;
+        if (j < 0) {
+            break;
+        }
+    }
+    
+    int dodatek = 0;
+    for (int i = 10000; i >= 0; i--) {
+        c[i] = (aa[i] + bb[i])%10 + dodatek;
+        dodatek = (aa[i] + bb[i])/10;
+    }
+    
+    bool najdenoPrvo = false;
+    for (int i = 0; i < 10000; i++) {
+        if (c[i] != 0) {
+            najdenoPrvo = true;
+        }
+        if (najdenoPrvo == true) {
+            printf("%d", c[i]);
+        }
+    }
+    printf("\n");
+ 
+    
+    
+}
+
