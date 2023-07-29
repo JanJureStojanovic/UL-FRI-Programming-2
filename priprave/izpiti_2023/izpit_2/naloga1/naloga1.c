c8f55f8399c440fc4cb0d60676d7ff1dc0b67740
Jan Jure Stojanovič <janjures@gmail.com>
2023-07-29 23:08:11 +0200

    Add hidden test cases

--- priprave/izpiti_2023/izpit_2/naloga1/naloga1.c
+++ priprave/izpiti_2023/izpit_2/naloga1/naloga1.c
@@ -14,7 +14,38 @@
 // po potrebi dopolnite ...
 
 int main(int argc, char** argv) {
-    // dopolnite ...
+    
+    FILE* in = fopen(argv[1], "rb");
+    FILE* p = fopen(argv[1], "rb");
+    FILE* out = fopen(argv[2], "wb");
+    
+    int c;
+    int n = 0;
+    while((c = fgetc(in)) != EOF) {
+    	n++;
+    }
+    
+    int* znaki = malloc(n*sizeof(int));
+    
+    int s;
+    int i = 0;
+    while((s = fgetc(p)) != EOF) {
+    	znaki[i] = s;
+    	i++;
+    }
+    
+    int prejsnji = -66767;
+    
+    for (int i = 0; i < n; i++) {
+    	if (znaki[i] != prejsnji) {
+    		fputc(znaki[i], out);
+    	}
+    	prejsnji = znaki[i];
+    }
+    
+    fclose(in);
+    fclose(p);
+    fclose(out);
 
     return 0;
 }
