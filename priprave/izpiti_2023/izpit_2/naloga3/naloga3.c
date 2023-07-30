6d9a19de320c04bca2d7a9a8ac537dc7b2632edb
Jan Jure Stojanovič <janjures@gmail.com>
2023-07-30 15:17:01 +0200

    3rd done, it all works

--- priprave/izpiti_2023/izpit_2/naloga3/naloga3.c
+++ priprave/izpiti_2023/izpit_2/naloga3/naloga3.c
@@ -26,9 +26,71 @@
 // po potrebi dopolnite ...
 
 Vozlisce* diagonala(Vozlisce* start, int* vsota) {
-    // popravite / dopolnite ...
-    return NULL;
+    
+    
+    Vozlisce* a = start;
+    Vozlisce* b = start;
+    
+    int w = 0;
+    while (a != NULL) {
+        w++;
+        a = a-> desno;
+    }
+    
+    int h = 0;
+    while (b != NULL) {
+        h++;
+        b = b -> dol;
+    }
+    
+    int* vrednosti = (h > w) ? malloc(w*sizeof(int)) : malloc(h*sizeof(int));
+    int dolzina = (h > w) ? w : h;
+    
+    Vozlisce* premikDol = start;
+    for (int i = 0; i < dolzina; i++) {
+        Vozlisce* premikDesno = premikDol;
+        
+        for (int j = 0; j < w - 1; j++) {
+            premikDesno = premikDesno->desno;
+        }
+        // Dodamo vrenost
+        vrednosti[i] = premikDesno->vsebina;
+        // Premaknemo se dol
+        premikDol = premikDol->dol; 
+        
+        if (premikDol == NULL) {
+            break;
+        }
+        
+        // Premikali se bomo eno manj desno
+        w = w - 1;
+        if (w == 0) { // Smo konec
+            break;
+        }
+    }
+    
+    Vozlisce* trenutno = malloc(sizeof(Vozlisce));
+    Vozlisce* out = trenutno;
+    int sum = 0;
+    for (int i = 0; i < dolzina; i++) {
+        sum += vrednosti[i];
+        Vozlisce* naslednje = malloc(sizeof(Vozlisce));
+        
+        trenutno->vsebina = vrednosti[i];
+        
+        
+        if (i != dolzina-1) {
+            trenutno->desno = naslednje;
+        } else {
+            break;
+        }
+        trenutno = trenutno->desno;
+    }
+    
+    *vsota = sum;
+    return out;
 }
+        
 
 //=============================================================================
 
