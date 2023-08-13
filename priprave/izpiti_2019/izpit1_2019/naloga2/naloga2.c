f86a9222bd2da5599465a92198ce6ee44f3b9fc6
Jan Jure Stojanovič <janjures@gmail.com>
2023-08-13 14:37:52 +0200

    2. naloga DONE!

--- priprave/izpiti_2019/izpit1_2019/naloga2/naloga2.c
+++ priprave/izpiti_2019/izpit1_2019/naloga2/naloga2.c
@@ -3,9 +3,33 @@
 #include <stdbool.h>
 #include <string.h>
 
+#include "naloga2.h"
+
 
 Vozlisce* obrni(Vozlisce* zacetek) {
+    
+    Vozlisce* i = zacetek;
+    
+    Vozlisce* prejsnje = NULL;
+    
+    Vozlisce* novo;
 
+    
+    while (i != NULL) {
+        
+        novo = malloc(1*sizeof(Vozlisce));
+        
+        novo->podatek = i->podatek;
+        
+        novo->naslednje = prejsnje;
+        
+        prejsnje = novo;
+        
+        i = i->naslednje;
+                 
+    }
+    
+    return novo;  
 
 }
 
