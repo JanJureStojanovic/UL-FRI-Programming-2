a8a6f601cf90687b1cbcc855f2738f7f6ebcf227
Jan Jure Stojanovič <janjures@gmail.com>
2023-08-15 19:23:44 +0200

    Allocation errors so only 5/8

--- priprave/izpiti_2022/OnlineRok2022/naloga3/naloga3.c
+++ priprave/izpiti_2022/OnlineRok2022/naloga3/naloga3.c
@@ -24,37 +24,78 @@ test02..test08: "se nekaj dodatnih testov
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
-void generate(char *S, char *result, int n, int pos, int* count){
-    if (pos == n) {
-        printf("%s\n", result);
+void izpis(char* niz, int n) {
+    for (int j = 0; j < n; j++) {
+        printf("%c", niz[j]);
+    }
+    printf("\n");
+}
+
+bool okStZnakov(int* t, int x) {
+    for (int j = 0; j < x; j++) {
+        if (t[j] > 3) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void okPermutacije(char* znaki, char* niz, int* uporabljeniZnaki, int d, int n, int i) {
+// i nam pove kje se nahajamo na nizu (katero crko bomo zapisali na niz dolzine n)
+    
+    // Smo zapolnili niz, lahko ga izpisemo;
+    if (i == n) {
+        if (okStZnakov(uporabljeniZnaki, d) == true) {
+            izpis(niz, n);
+            free(niz);
+            free(uporabljeniZnaki);
+            return;
+        }
+    }
+    
+    if (okStZnakov(uporabljeniZnaki, d) == false) {
+        free(niz);
+        free(uporabljeniZnaki);
         return;
     }
-
-    for (int i = 0; i < strlen(S); i++){
-        if (count[i] < 3) {
-            result[pos] = S[i];
-            count[i]++;
-
-            generate(S, result, n, pos + 1, count);
-
-            // Backtracking
-            count[i]--;
-            result[pos] = '\0';
+    
+    // Na i-to mesto damo vsak znak in gremo naprej
+    for (int l = 0; l < d; l++) {
+        // Prekopiramo uporabljene znake
+        int* noviUporabljeniZnaki = calloc(d, sizeof(char));
+        for (int k = 0; k < d; k++) {
+            noviUporabljeniZnaki[k] = uporabljeniZnaki[k];
         }
+        
+        // Prekopiramo niz
+        char* noviNiz = calloc(n, sizeof(char));
+        strcpy(noviNiz, niz);
+        
+        noviNiz[i] = znaki[l];
+        noviUporabljeniZnaki[l]++;
+        
+        okPermutacije(znaki, noviNiz, noviUporabljeniZnaki, d, n, i + 1);
     }
 }
 
-int main(){
-    int d, n;
-    char S[51];
+int main() {
 
-    scanf("%d %s %d", &d, S, &n);
-
-    char* result = calloc((n+1), sizeof(char));
-    int* count = calloc(d, sizeof(int));
-
-    generate(S, result, n, 0, count);
-
-    return 0;
+    int d;
+    scanf("%d ", &d);
+    
+    char* znaki = calloc(d, sizeof(char));
+    for (int i = 0; i < d; i++) {
+        znaki[i] = getchar();
+    }
+    
+    int n;
+    scanf(" %d", &n);
+    
+    char* niz = calloc(n, sizeof(char));
+    int* uporabljeniZnaki = calloc(d, sizeof(int));
+    
+    okPermutacije(znaki, niz, uporabljeniZnaki, d, n, 0);
+  
 }
