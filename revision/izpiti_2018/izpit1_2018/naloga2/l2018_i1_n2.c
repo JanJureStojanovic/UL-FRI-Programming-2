33f06982eea5e8c80b65ce58e2149e03c2a54d7d
Jan Jure Stojanovič <janjures@gmail.com>
2023-08-18 22:29:36 +0200

    Practice

--- revision/izpiti_2018/izpit1_2018/naloga2/l2018_i1_n2.c
+++ revision/izpiti_2018/izpit1_2018/naloga2/l2018_i1_n2.c
@@ -1,4 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "naloga2.h"
 
+uchar* preberi(char* imeDat, int* sirina, int* visina, int* stBajtov) {
+
+    FILE* input = fopen(imeDat, "rb");
+    
+    char P6[3];
+    int width, height, max_color;
+    fscanf(input, "%2s %d %d %d", P6, &width, &height, &max_color);
+    
+    *visina = height;  // Update the value pointed to by visina
+    *sirina = width;   // Update the value pointed to by sirina
+    
+    fgetc(input);
+    
+    unsigned char* colors = malloc(3 * width * height * sizeof(uchar));
+    fread(colors, sizeof(unsigned char), 3 * width * height, input);
+    
+    *stBajtov = 3 * width * height;
+    
+    fclose(input);  // Don't forget to close the file
+    
+    return colors;
+}
+
+int sivina(uchar* pike, int sirina, int visina, int vrstica, int stolpec) {
+    
+    int ix = (vrstica*sirina*3) + stolpec*3;
+    
+    int sivko = (pike[ix] + pike[ix + 1] + pike[ix + 2])/3;
+    
+    return sivko;
+}
+
+int main() {
+    
+    return 0;
+    
+}
