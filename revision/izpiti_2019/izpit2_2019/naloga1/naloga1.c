a51fd23445cecd8eab8a67994957e65fb3193b34
Jan Jure Stojanovič <janjures@gmail.com>
2023-08-19 09:18:16 +0200

    Better solution

--- revision/izpiti_2019/izpit2_2019/naloga1/naloga1.c
+++ revision/izpiti_2019/izpit2_2019/naloga1/naloga1.c
@@ -1,55 +1,8 @@
 #include <stdio.h>
-
-void count_color_components(const char *filename, int *counts) {
-
-    FILE *input_file = fopen(filename, "rb");
-
-    fseek(input_file, 0, SEEK_END);
-    long file_size = ftell(input_file);
-    rewind(input_file);
-
-    int num_pixels = file_size / 3;
-
-    int r_gt_0_g_0_b_0_count = 0;
-    int r_0_g_gt_0_b_0_count = 0;
-    int r_0_g_0_b_gt_0_count = 0;
-
-    unsigned char pixel[3];
-
-    for (int i = 0; i < num_pixels; i++) {
-        fread(pixel, sizeof(unsigned char), 3, input_file);
-        unsigned char r = pixel[0];
-        unsigned char g = pixel[1];
-        unsigned char b = pixel[2];
-
-        if (r > 0 && g == 0 && b == 0) {
-            r_gt_0_g_0_b_0_count++;
-        } else if (r == 0 && g > 0 && b == 0) {
-            r_0_g_gt_0_b_0_count++;
-        } else if (r == 0 && g == 0 && b > 0) {
-            r_0_g_0_b_gt_0_count++;
-        }
-    }
-
-    fclose(input_file);
-
-    counts[0] = r_gt_0_g_0_b_0_count;
-    counts[1] = r_0_g_gt_0_b_0_count;
-    counts[2] = r_0_g_0_b_gt_0_count;
-}
-
-void write_output(const char *filename, const int *counts) {
-
-    FILE *output_file = fopen(filename, "w");
-
-    for (int i = 0; i < 3; i++) {
-        fprintf(output_file, "%d\n", counts[i]);
-    }
-
-    fclose(output_file);
-}
+#include <stdlib.h>
 
 int main() {
+
     char input_filename[21];
     int n;
     char output_filename[21];
@@ -57,12 +10,32 @@ int main() {
     scanf("%s", input_filename);
     scanf("%d", &n);
     scanf("%s", output_filename);
+    
+    FILE* input = fopen(input_filename, "rb");
+    FILE* output = fopen(output_filename, "w");
+    
+    unsigned char* bajti = malloc(n*3*sizeof(unsigned char));
+    fread(bajti, sizeof(unsigned char), n*3, input);
+    
+    int r = 0;
+    int g = 0;
+    int b = 0;
+    
+    for (int i = 0; i < n*3; i += 3) {
+        if (bajti[i] != 0 && bajti[i+1] == 0 && bajti[i+2] == 0) {
+            r++;
+        }
+        if (bajti[i] == 0 && bajti[i+1] != 0 && bajti[i+2] == 0) {
+            g++;
+        }
+        if (bajti[i] == 0 && bajti[i+1] == 0 && bajti[i+2] != 0) {
+            b++;
+        }
+    }
+    
+    fprintf(output, "%d\n%d\n%d\n", r, g, b);
 
-    int counts[3];
-    count_color_components(input_filename, counts);
-
-    write_output(output_filename, counts);
-
+    
     return 0;
 }
 
